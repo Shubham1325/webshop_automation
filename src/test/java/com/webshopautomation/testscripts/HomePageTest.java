2902897acbcff6a52b5b98435f975128e0c7604e
Shubham Kumar Singh <shubhamkumarsingh1319@gmail.com>
2025-11-19 20:17:10 +0530

    Created new Test Script

---
+++ src/test/java/com/webshopautomation/testscripts/HomePageTest.java
@@ -0,0 +1,11 @@
+package com.webshopautomation.testscripts;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	@Test
+	public void homePageTest() {
+		System.out.println("This is to test Home Page");
+	}
+}
